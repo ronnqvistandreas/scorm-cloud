944b4672a1981bb9167fbe6865080e7be3b11a0c
Andreas Rönnqvist <andreas.ronnqvist@xdin.com>
2013-10-11 09:38:02 +0200

    Implement get_course_tags

--- lib/scorm_cloud/tagging_service.rb
+++ lib/scorm_cloud/tagging_service.rb
@@ -1,9 +1,19 @@
 module ScormCloud
 	class TaggingService < BaseService
-		not_implemented :get_course_tags, :set_course_tags, :add_course_tag,
+		not_implemented :set_course_tags, :add_course_tag,
 				:remove_course_tag, :get_learner_tags, :set_learner_tags, :add_learner_tag,
 				:remove_learner_tag, :get_registration_tags, :set_registration_tags,
 				:add_registration_tag, :remove_registration_tag
-	end
+
+    def get_course_tags(courseid)
+      xml = sc.tagging.connection.call("rustici.tagging.getCourseTags", :courseid => courseid)
+      tags = []
+      xml.elements["/rsp/tags"].each{|t|
+        tags << t[0].to_s
+      }
+      tags
+    end
+  end
+
 end
 
